68a44d9074fba6a6f5e579e448a0ebe22c967f28
Đức Tiến <ductien.uit@gmail.com>
2018-10-13 13:11:15 +0700

    add transition

--- app/src/main/java/com/trafficanalysics/MainActivity.java
+++ app/src/main/java/com/trafficanalysics/MainActivity.java
@@ -3,23 +3,30 @@ package com.trafficanalysics;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.view.animation.FastOutLinearInInterpolator;
+import android.support.v4.view.animation.LinearOutSlowInInterpolator;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.FrameLayout;
 
 import com.luseen.luseenbottomnavigation.BottomNavigation.BottomNavigationItem;
 import com.luseen.luseenbottomnavigation.BottomNavigation.BottomNavigationView;
 import com.luseen.luseenbottomnavigation.BottomNavigation.OnBottomNavigationItemClickListener;
+import com.transitionseverywhere.Fade;
+import com.transitionseverywhere.TransitionManager;
+import com.transitionseverywhere.TransitionSet;
+import com.transitionseverywhere.extra.Scale;
 
 public class MainActivity extends AppCompatActivity {
 
     BottomNavigationView bottomNavigationView;
     ActionBar toolbar;
+    FrameLayout contentContainer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
 
         addControl();
 
@@ -39,14 +46,16 @@ public class MainActivity extends AppCompatActivity {
 
         if (bottomNavigationView != null) {
             bottomNavigationView.isWithText(true);
-            // bottomNavigationView.activateTabletMode();
+            //bottomNavigationView.activateTabletMode();
             bottomNavigationView.isColoredBackground(false);
             bottomNavigationView.setTextActiveSize(getResources().getDimension(R.dimen._9sdp));
             bottomNavigationView.setTextInactiveSize(getResources().getDimension(R.dimen._9sdp));
-            //bottomNavigationView.setItemActiveColorWithoutColoredBackground(ContextCompat.getColor(this, R.color.activeItem));
+            bottomNavigationView.setItemActiveColorWithoutColoredBackground(ContextCompat.getColor(this, R.color.activeItem));
             //bottomNavigationView.setFont(Typeface.createFromAsset(getApplicationContext().getAssets(), "fonts/"));
 
         }
+
+        contentContainer = findViewById(R.id.contentContainer);
 
         addNavItem();
 
@@ -56,16 +65,24 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView.setOnBottomNavigationItemClickListener(new OnBottomNavigationItemClickListener() {
             @Override
             public void onNavigationItemClick(int index) {
+                TransitionSet set = new TransitionSet()
+                        .addTransition(new Scale(0.7f))
+                        .addTransition(new Fade())
+                        .setInterpolator(new LinearOutSlowInInterpolator());
+
                 switch (index) {
                     case 0:
+                        TransitionManager.beginDelayedTransition(contentContainer, set);
                         toolbar.setTitle("News");
-                        loadFragment(new ChatbotFragment());
+                        loadFragment(new NewsFragment());
                         break;
                     case 1:
+                        TransitionManager.beginDelayedTransition(contentContainer, set);
                         toolbar.setTitle("Traffic Message");
                         loadFragment(new ChatbotFragment());
                         break;
                     case 2:
+                        TransitionManager.beginDelayedTransition(contentContainer, set);
                         toolbar.setTitle("Map Traffic");
                         loadFragment(new MapFragment());
                         break;
@@ -76,11 +93,11 @@ public class MainActivity extends AppCompatActivity {
 
     private void addNavItem() {
         BottomNavigationItem dateItem = new BottomNavigationItem
-                ("News", ContextCompat.getColor(this, R.color.colorAccent), R.drawable.ic_match);
+                ("News", ContextCompat.getColor(this, R.color.colorAccent), R.drawable.ic_news);
         BottomNavigationItem channelItem = new BottomNavigationItem
-                ("Traffic Message", ContextCompat.getColor(this, R.color.colorPrimary), R.drawable.ic_live_tv);
+                ("Traffic Message", ContextCompat.getColor(this, R.color.colorPrimary), R.drawable.ic_messages);
         BottomNavigationItem newItem = new BottomNavigationItem
-                ("Map Traffic", ContextCompat.getColor(this, R.color.colorPrimary), R.drawable.ic_news);
+                ("Map Traffic", ContextCompat.getColor(this, R.color.colorPrimary), R.drawable.ic_maps);
 
         bottomNavigationView.addTab(dateItem);
         bottomNavigationView.addTab(channelItem);
